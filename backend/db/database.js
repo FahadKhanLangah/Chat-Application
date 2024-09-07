2290ad25fdb229e53ef7435519179e4b882acb19
Fahad Khan Langah <fahadkhanavoid@gmail.com>
2024-09-07 11:32:21 +0500

    connected db

---
+++ backend/db/database.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const connectDB = async()=>{
+  await mongoose.connect(process.env.DB_URI).then(()=>{
+    console.log('database connected')
+  }).catch((err)=>{
+    console.log(err)
+  })
+}
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
-import express from 'express'
-
+import express from 'express';
+import dotenv from 'dotenv'
+import connectDB from './db/database.js';
+dotenv.config({})
 const app = express();
 
-const PORT = 4000
+const PORT = process.env.PORT || 4500;
 
 app.listen(PORT,()=>{
-  console.log('server is running')
+  connectDB();
+  console.log(`server is running ${PORT}`)
 })
